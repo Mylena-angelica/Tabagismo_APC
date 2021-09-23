b794b37dad57a8addaf271f104434d7902870d02
Mylena Angélica <86479209+Mylena-angelica@users.noreply.github.com>
2021-09-23 10:46:33 -0300

    Update tabaco_producao.py

--- tabaco_producao.py
+++ tabaco_producao.py
@@ -14,23 +14,23 @@ producao=[] # Lista da quantidade de tabaco produzida
 
 for linha in dados_array:  #Selecionando os dados da tabela
     if linha[2]== 2018: # A priori, a seleção do ano será assim. No futuro será por meio de um dropdown.
-        pais.append(linha[1]) 
+        pais.append(linha[1]) # append serve para criar as listas.
         ano.append(linha[2]) 
         producao.append(linha[3])
 
 for codes in pais:    # Laço para excluir os dados que não se aplica a ISO_alpha (Dados por continente)
     if not type(codes)== str: 
         if math.isnan(float(codes)): #Condição para usar a função a baixo, quando aparece nan (ou seja, não existe o código ISO_alpha na tabela)
-            index=pais.index(codes)
-            del pais[index]
-            del ano[index]
-            del producao[index]
+            index=pais.index(codes) #Função index serve para procurar um elemento
+            del pais[index] #Deletando os dados relacionados ao nan
+            del ano[index] #Deletando os dados relacionados anos dos codes que apresentam nan
+            del producao[index] #Deletando os dados relacionados à produção que apresenta nan
             
     elif codes=='OWID_WRL' #Excluindo os dados relacionados ao mundo como um todo, os dados ligados a str OWID_WRL
-        index=pais.index(codes)
-            del pais[index] 
-            del ano[index]
-            del producao[index]
+        index=pais.index(codes) 
+            del pais[index] #Deletando os dados relacionados ao nan
+            del ano[index] #Deletando os dados relacionados ao nan
+            del producao[index] #Deletando os dados relacionados ao nan
 
 #Configurando o gráfico
 
@@ -41,8 +41,8 @@ fig = go.Figure(data=go.Choropleth( #Recuperando dados do gráfico na base de da
     colorscale = 'plasma', # Paleta de cores do mapa
     autocolorscale=False, # O false determina que você escolhe a paleta de cores
     reversescale=True, # Quando verdadeira, associa as cores aos valores do z
-    marker_line_color='darkgray', #Questões de configuração
-    marker_line_width=0.5, #Questões de configuração
+    marker_line_color='darkgray', # Cor das fronteiras
+    marker_line_width=0.5, # Espessura das fronteiras
     colorbar_tickprefix = 'Toneladas', #Prefixo da coluna da legenda
     colorbar_title = 'Produção em <br>toneladas', #Título da legenda de cores
 ))
@@ -50,8 +50,8 @@ fig = go.Figure(data=go.Choropleth( #Recuperando dados do gráfico na base de da
 fig.update_layout(
     title_text='Produção mundial de tabaco', #Título do gráfico
     geo=dict(
-        showframe=False,
-        showcoastlines=False,
+        showframe=False, # Borda do mapa, quando False não irá aparecer
+        showcoastlines=False, # Fronteiras costeiras, quando False não são marcadas
         projection_type='equirectangular' #Tipo de projeção do mapa
     ),
     annotations = [dict(
@@ -61,7 +61,7 @@ fig.update_layout(
         yref='paper', #Referência do eixo X
         text='Fonte: <a href="https://ourworldindata.org/grapher/tobacco-production">\
             Our world in Data</a>',  #Fonte
-        showarrow = False
+        showarrow = False #Questão estética
     )]
 )
 
