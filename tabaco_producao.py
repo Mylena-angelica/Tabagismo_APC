3d09ddc8ef16148c1c6df39fe83affca77381f72
Mylena Angélica <86479209+Mylena-angelica@users.noreply.github.com>
2021-09-23 10:19:50 -0300

    Update tabaco_producao.py

--- tabaco_producao.py
+++ tabaco_producao.py
@@ -18,9 +18,9 @@ for linha in dados_array:  #Selecionando os dados da tabela
         ano.append(linha[2]) 
         producao.append(linha[3])
 
-for codes in pais:    # Laço pa excluir os dados que não se aplica a ISO_alpha (Dados por continente)
+for codes in pais:    # Laço para excluir os dados que não se aplica a ISO_alpha (Dados por continente)
     if not type(codes)== str: 
-        if math.isnan(float(codes)): #Condição para usar a função a baixo, quando aparece nan
+        if math.isnan(float(codes)): #Condição para usar a função a baixo, quando aparece nan (ou seja, não existe o código ISO_alpha na tabela)
             index=pais.index(codes)
             del pais[index]
             del ano[index]
@@ -37,13 +37,13 @@ for codes in pais:    # Laço pa excluir os dados que não se aplica a ISO_alpha
 fig = go.Figure(data=go.Choropleth( #Recuperando dados do gráfico na base de dados e o tipo de dados
     locations = pais, #Utiliza os ISO_Alpha (Codes) para localizar os países no gráfico
     z = producao, #Utiliza os dados de produção, é o que relaciona as cores do gráfico
-    text = ano, #O ano excolhido
+    text = ano, #O ano escolhido
     colorscale = 'plasma', # Paleta de cores do mapa
     autocolorscale=False, # O false determina que você escolhe a paleta de cores
-    reversescale=True, # QUando verdadeira, cria associa as cores a valores
-    marker_line_color='darkgray',
-    marker_line_width=0.5,
-    colorbar_tickprefix = 'Toneladas', 
+    reversescale=True, # Quando verdadeira, associa as cores aos valores do z
+    marker_line_color='darkgray', #Questões de configuração
+    marker_line_width=0.5, #Questões de configuração
+    colorbar_tickprefix = 'Toneladas', #Prefixo da coluna da legenda
     colorbar_title = 'Produção em <br>toneladas', #Título da legenda de cores
 ))
 
