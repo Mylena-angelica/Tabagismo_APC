70c00fc51b58d5290b9201b949bfe25d9feccd56
Joao2021apc <90392649+Joao2021apc@users.noreply.github.com>
2021-10-04 18:18:23 -0300

    Update Mortes_por_genero_sem_botao.py

--- Mortes_por_genero_sem_botao.py
+++ Mortes_por_genero_sem_botao.py
@@ -1,12 +1,17 @@
-from numpy import nan
 import pandas as pd
 import plotly.graph_objects as go
 
 dados = pd.read_csv("plotly/comparing-the-share-of-men-and-women-who-are-smoking.csv", sep=",")
-#print(dados)
+
+'''
+print(dados)
+'''
 
 dados_array = dados.values
-#print(dados_array)
+
+'''
+print(dados_array)
+'''
 
 sigla = []
 ano = []
@@ -17,14 +22,21 @@ pais3 = []
 Texto1 = []
 Texto2 = []
 
-for linha in dados_array:  #verificar se o nome do país por linha para tirar as repetições e 
-    if not linha[0] in pais3:  #armazenar apenas uma vez o nome de cada pais em pais3
+'''O loop abaixo tem a função de verificar se o nome do país por linha para tirar as repetições e
+armazenar apenas uma vez o nome de cada pais em pais3
+'''
+
+for linha in dados_array:   
+    if not linha[0] in pais3:  
         pais3.append(linha[0])
         sigla.append(linha[1])
-
-
-for linha in dados_array:      #verificar o nome as datas e só considerar as informações entre oa anos:
-                               # 2007, 2010, 2012, 2014, 2016 e 2018
+        
+'''
+Esse loop abaixo tem a função de verificar o nome as datas e só considerar as informações entre oa anos:
+2007, 2010, 2012, 2014, 2016 e 2018
+'''             
+        
+for linha in dados_array:      
     if (linha[2] == 2007 or linha[2] >= 2010 and linha[2] <= 2018 and (linha[2] % 2 == 0)) and linha[0] == 'Brazil':
           
         ano.append(linha[2])
