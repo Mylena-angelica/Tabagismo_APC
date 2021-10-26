cfabd38578914aa9a87a3a015c1bd1cf5eb9409d
anajuliafreitas <90220502+anajuliafreitas@users.noreply.github.com>
2021-10-26 17:31:37 -0300

    Update taxa_de_mortes_por_idade_dash.py

--- taxa_de_mortes_por_idade_dash.py
+++ taxa_de_mortes_por_idade_dash.py
@@ -21,28 +21,29 @@ app.layout = html.Div([     # o layout da página é formado por vários compone
         dcc.Graph(id = 'the_graph')    # o dcc.Graph irá receber o gráfico da função 'the_graph' que foi endereçada no componente do output
     ]),
 
-    html.Div([
-        dcc.Input(id ='input_state', type = 'number', inputMode = 'numeric', value = 2017,   
-                max = 2017, min = 1990, required = True), 
+    html.Div([     # id : identifica o input; type : parâmetro do que será escrito, um número (ano)
+        dcc.Input(id ='input_state', type = 'number', inputMode = 'numeric', value = 2017,    # inputmode : identifica a linguagem utilizada, o numeral; value : valor em que o gráfico será gerado inicialmente 
+                max = 2017, min = 1990, required = True),    # required : aparecerá um retanângulo vermelho no botão caso não tenha selecionado um ano ou o ano escolhido não tenha dados 
 
-        html.Button(id = 'submit_button', n_clicks = 0, children = 'Gerar gráfico'),
+        html.Button('Gerar gráfico', id = 'submit_button', n_clicks = 0),  # n_clicks : quantidade de pesquisas feitas (iniciamnente nenhuma)
         html.Div(id = 'output_state'), 
-    ], style = {'text-align': 'center'})
+    ], style = {'text-align': 'center'})   # posição do texto
 
 ])
 
 # configurações do callback: identificação do output, input e state
 @app.callback( 
-    Output(component_id = 'output_state', component_property = 'children'), 
-    Output(component_id = 'the_graph', component_property = 'figure'),
-    [Input(component_id = 'submit_button', component_property = 'n_clicks')], 
-    [State(component_id = 'input_state', component_property = 'value')]  
+    Output(component_id = 'output_state', component_property = 'children'),     # declarando qual será o output
+    Output(component_id = 'the_graph', component_property = 'figure'),          # declarando o id do componente e sua propriedade
+    [Input(component_id = 'submit_button', component_property = 'n_clicks')],   # declarando que o submit_button, relaciona-se com o n_clicks
+    [State(component_id = 'input_state', component_property = 'value')]         # declarando O input_state relaciona-se com value
+    #State é diferente de Input, permitindo que você declare valores extras sem disparar o callback
 )
 
 # criando uma função para o botão que vai gerar o gráfico
 def update_output(num_clicks, val_selected):
     if val_selected is None: 
-        raise PreventUpdate 
+        raise PreventUpdate    # se não for selecionado nenhum valor no botão o gráfico não atualizará
     else:
         dados = df.values   # a propriedade values lê os dados em dataframe e passa para dentro de uma lista de listas
          
@@ -56,14 +57,16 @@ def update_output(num_clicks, val_selected):
             if 'Brazil' == linha[0]:
                 dados_brasil.append(linha)
         
-        # filtragem para apenas o ano escolhido
-        for dado in dados_mundo:
-            if dado[2] == val_selected:
-                dados_mundo_ano = dado
+        # filtragem para apenas o ano escolhido usando a função filtrar_ano_selecionado 
+        def filtrar_ano_selecionado(dados_todos_anos):
+            for dado in dados_todos_anos:
+                if dado[2] == val_selected:
+                    dados_ano_selecionado = dado
+            
+            return dados_ano_selecionado       
 
-        for dado in dados_brasil:
-            if dado[2] == val_selected:
-                dados_brasil_ano = dado  
+        dados_brasil_ano = filtrar_ano_selecionado(dados_brasil)   
+        dados_mundo_ano = filtrar_ano_selecionado(dados_mundo)        
         
         # definição da função que fará a filtragem dos dados do Brasil e do mundo
         def filtrar_dados(dados_ano_array):
@@ -102,25 +105,26 @@ def update_output(num_clicks, val_selected):
             name = 'Brasil',
             x = idades,
             y = mortes_idade_brasil,
-            text = mortes_idade_brasil,
-            textposition = 'auto'
+            text = mortes_idade_brasil,   # textro mostrando as taxas em cada barra
+            textposition = 'auto'   # localização do texto dentro da barra
         )
 
         barra2 = go.Bar(
             name = 'Mundo',
             x = idades,
             y = mortes_idade_mundo,
-            text = mortes_idade_mundo,
-            textposition = 'auto'
+            text = mortes_idade_mundo,   # texto mostrando as taxas em cada barra
+            textposition = 'auto'   # localização do texto dentro da barra
         )
 
         grafico = go.Figure([barra1, barra2])
 
         # layout e título
         grafico.update_layout(barmode = 'group',  # layout de barras uma do lado da outra
-        title = 'Taxa de mortes prematuras devido ao tabagismo no ano de {}'.format(val_selected)
-        )
+        title = 'Taxa de mortes por idade, devido ao tabagismo <br><sup> Medida pelo número de mortes prematuras, devido ao tabagismo, por 100.000 indivíduos em um determinado grupo demográfico.</sup>' 
+        )     # colocando um título e subtítulo usando html
         
+        # a função retornará o gráfico e a frase abaixo mencionando o ano selecionado(val_selected) e o número de pesquisas(num_clicks)
         return ('O ano selecionado foi {} e você já fez  \
                  {} pesquisas'.format(val_selected, num_clicks), grafico)
 
